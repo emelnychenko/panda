17288715f2cd9972e2cfd5fea686877ad4821525
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-08-27 14:51:28 +0300

    Processor Framework

---
+++ Framework/Http/ProcessorAbstract.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  Panda PHP Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Http;
+
+use Panda\Foundation\Http\ProcessorEventAbstract;
+
+/**
+ *  Http Processor Abstract
+ *
+ *  @subpackage Http
+ */
+abstract class ProcessorAbstract extends ProcessorEventAbstract implements ProcessorInterface
+{
+    # extends ...
+}
+
---
+++ Framework/Http/ProcessorInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda Framework
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Http;
+
+/**
+ *  Http Processor Interface
+ *
+ *  @subpackage Http
+ */
+interface ProcessorInterface
+{
+    # implements ...
+}
