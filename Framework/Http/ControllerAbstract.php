e8cc84f14d0d5f833a0cd1f1ed4f8368e39d3797
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-08-27 14:51:00 +0300

    Controller Framework

---
+++ Framework/Http/ControllerAbstract.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  Panda PHP Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Http;
+
+use Panda\Foundation\Http\ControllerNativeAbstract;
+
+/**
+ *  Http Controller Abstract
+ *
+ *  @subpackage Http
+ */
+abstract class ControllerAbstract extends ControllerNativeAbstract implements ControllerInterface
+{
+    # extends ...
+}
+
---
+++ Framework/Http/ControllerInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda Framework
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Http;
+
+/**
+ *  Http Controller Interface
+ *
+ *  @subpackage Http
+ */
+interface ControllerInterface
+{
+    # implements ...
+}
