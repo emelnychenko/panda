c97c05f12e2722920fb7533900dbaea37a69463f
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-09-02 21:36:41 +0300

    Restruct Writeble Essence

---
+++ source/Essence/Writeable.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Essence;
+
+use Panda\Alloy\FactoryInterface as Factory;
+
+/**
+ *  Writeable Essence
+ *
+ *  @subpackage Essence
+ */
+class Writeable extends WriteableAbstract implements Factory
+{
+    /**
+     *  New instance.
+     *
+     *  @var mixed  $shared
+     */
+    public function __construct($shared = null)
+    {
+        if (isset($shared)) {
+            $this->shared = is_array($shared) ? $shared : func_get_args();
+        }
+    }
+
+    /**
+     *  Factory instance.
+     *
+     *  @var mixed  $shared
+     */
+    public static function factory($shared = null)
+    {
+        return new static($shared);
+    }
+
+    /**
+     *  Return whole shared result.
+     *
+     *  @return array
+     */
+    public function all()
+    {
+        return $this->shared;
+    }
+}
---
+++ source/Essence/WriteableAbstract.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Essence;
+
+/**
+ *  Writeable Essence Abstract
+ *
+ *  @subpackage Essence
+ */
+abstract class WriteableAbstract extends ReadableAbstract
+{
+    /**
+     *  Set container by key.
+     *
+     *  @var mixed $keys
+     *  @var mixed $equal
+     *
+     *  @return \Panda\Essence\WriteableAbstract
+     */
+    public function set($keys, $equal = null)
+    {
+        if (is_array($keys) && $equal === null) {
+            $this->shared = array_replace($this->shared, $keys);
+        } else {
+            $this->shared[$keys] = $equal;
+        }
+
+        return $this;
+    }
+
+    /**
+     *  Set magic shared by key.
+     *
+     *  @var scalar $key
+     *  @var mixed $equal
+     *
+     *  @return \Panda\Essence\WriteableAbstract
+     */
+    public function __set($key, $equal)
+    {
+        return $this->set($key, $equal);
+    }
+
+    /**
+     *  Set shared array data.
+     *
+     *  @var array $shared
+     *
+     *  @return \Panda\Essence\WriteableAbstract
+     */
+    public function replace(array $shared = [])
+    {
+        $this->shared = array_replace($this->shared, $shared);
+
+        return $this;
+    }
+
+    /**
+     *  Set magic shared by key.
+     *
+     *  @var mixed $key
+     *  @var mixed $equal
+     *
+     *  @return \Panda\Essence\WriteableAbstract
+     */
+    public function push($keys, $equal = null)
+    {
+        $makeable = function($key, $equal) {
+            if (strpos($key, '.') === false) {
+                $this->shared[$key] = $equal;
+            } else {   
+                $this->shared = $this->__push(($chain = explode('.', $key)), $equal, 0, count($chain), $this->shared);
+            }
+        };
+
+        if (is_array($keys) && $equal === null) {
+            foreach ($keys as $key => $equal) {
+                call_user_func($makeable, $key, $equal);
+            }
+
+            return $this;
+        }
+
+        call_user_func($makeable, $keys, $equal);
+
+        return $this;
+    }
+
+    /**
+     *  Set magic shared by key.
+     *
+     *  @var scalar $key
+     *  @var mixed $equal
+     *
+     *  @return \Panda\Essence\WriteableAbstract
+     */
+    public function __push(array $keys, $value, $index, $limit = 0, $source)
+    {
+        if ($index >= $limit) return $value;
+        $source[$keys[$index]] = call_user_func(
+            __METHOD__, $keys, $value, $index + 1, $limit, isset($source[$keys[$index]]) ? $source[$keys[$index]] : []
+        );
+
+        return $source;
+    }
+}
