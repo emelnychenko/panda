f0aaa5dfb74e877de1d4f7b0a9738f7a6f1b477e
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-09-03 12:54:14 +0300

    Http Cookie Test and Cookie Entity

---
+++ source/Http/Cookie.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Http;
+
+/**
+ *  Http Cookie Interface
+ *
+ *  @subpackage Http
+ */
+class Cookie extends CookieAbstract
+{
+    # implements
+}
