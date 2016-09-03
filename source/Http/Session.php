dec54c449c587985f08d8e9bf1e22f2eb64bb2d3
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-09-03 22:35:27 +0300

    Session single layer model

---
+++ source/Http/Session.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ *  @since   v1.2.0
+ */
+
+namespace Panda\Http;
+
+/**
+ *  Http Session
+ *
+ *  @subpackage Http
+ */
+class Session extends SessionAbstract
+{
+    # implements
+}
