b2be8786b97dd507cbeb45c9a68826af9b9578dd
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-09-04 19:40:38 +0300

    Floated implementation

---
+++ source/Essence/Floated.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ *  @since   v1.2.0
+ */
+
+namespace Panda\Essence;
+
+/**
+ *  Floated Essence
+ *
+ *  @subpackage Essence
+ */
+class Floated extends FloatedAbstract
+{
+    //
+}
