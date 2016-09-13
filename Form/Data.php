8a54126e4521f96438fd44ddc2b8b6e29114a9ba
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-09-13 17:52:48 +0300

    Data override instance

---
+++ Form/Data.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ */
+
+namespace Panda\Form;
+
+/**
+ *  Panda Form
+ *
+ *  @subpackage Framework
+ */
+class Data extends DataAbstract
+{
+    //
+}
