df36255824ee98ad6bb3ea053e97c9740816f6bb
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2016-09-13 17:50:48 +0300

    Duplicate Factory

---
+++ Alloy/Factory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  Panda PHP Foundation, Packages and Framework
+ *
+ *  @package Panda
+ *  @author  Eugen Melnychenko
+ *  @since   v1.2.0
+ */
+
+namespace Panda\Alloy;
+
+/**
+ *  Factory Alloy
+ *
+ *  @subpackage Alloy
+ */
+interface Factory
+{
+    /**
+     *  Factory instance.
+     *
+     *  @return Factory
+     */
+    public static function factory();
+}
